971dea634ca6bef1517667023726677426699e53
Marcos Felipe Souza <mfelipesp@gmail.com>
2016-12-29 21:19:33 -0200

    fim

---
+++ Leitor QRCODE/ScannerViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ScannerViewController.swift
+//  Leitor QRCODE
+//
+//  Created by Marcos Felipe Souza on 29/12/16.
+//  Copyright © 2016 Marcos. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate  {
+    
+    let supportedCodeTypes = [AVMetadataObjectTypeUPCECode,
+                              AVMetadataObjectTypeCode39Code,
+                              AVMetadataObjectTypeCode39Mod43Code,
+                              AVMetadataObjectTypeCode93Code,
+                              AVMetadataObjectTypeCode128Code,
+                              AVMetadataObjectTypeEAN8Code,
+                              AVMetadataObjectTypeEAN13Code,
+                              AVMetadataObjectTypeAztecCode,
+                              AVMetadataObjectTypePDF417Code,
+                              AVMetadataObjectTypeQRCode]
+    
+    var captureSession:AVCaptureSession? //captura o video em tempo real
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
+    
+    
+    @IBOutlet weak var topbar: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        capture()
+        
+    }
+    
+    func moveViewToFront() -> Void {
+        //move as view para frente da camera
+        view.bringSubview(toFront: messageLabel)
+        view.bringSubview(toFront: topbar)
+    }
+
+    func greenBox() {
+
+        qrCodeFrameView = UIView()
+
+        if let qrCodeFrameView = qrCodeFrameView {
+            qrCodeFrameView.layer.borderColor = UIColor.green.cgColor
+            qrCodeFrameView.layer.borderWidth = 2
+            view.addSubview(qrCodeFrameView)
+            view.bringSubview(toFront: qrCodeFrameView)
+        }
+    }
+    
+    func capture(){
+        
+        let captureDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession = AVCaptureSession()
+            
+            // Set the input device on the capture session.
+            captureSession?.addInput(input)
+            
+            //Captura a saida da sessao
+            let captureMetadataOutput = AVCaptureMetadataOutput()
+            captureSession?.addOutput(captureMetadataOutput)
+            
+            //adiciona o delegate
+            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+            
+            //permite o uso da camera para que o usuario possa capturar e colocar em uma view
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+            videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+            videoPreviewLayer?.frame = view.layer.bounds
+            view.layer.addSublayer(videoPreviewLayer!)
+            
+            // Start video capture.
+            captureSession?.startRunning()
+                        
+            greenBox()
+            moveViewToFront()
+            
+        } catch {
+            print(error)
+            return
+        }
+    }
+    
+    @IBAction func goBackToHomeView(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+        
+        // Check if the metadataObjects array is not nil and it contains at least one object.
+        if metadataObjects == nil || metadataObjects.count == 0 {
+            qrCodeFrameView?.frame = CGRect.zero
+            messageLabel.text = "QR code não detectado"
+            return
+        }
+        
+        // Get the metadata object.
+        let metadataObj = metadataObjects[0] as! AVMetadataMachineReadableCodeObject
+        
+        if metadataObj.type == AVMetadataObjectTypeQRCode {
+            // If the found metadata is equal to the QR code metadata then update the status label's text and set the bounds
+            let barCodeObject = videoPreviewLayer?.transformedMetadataObject(for: metadataObj)
+            qrCodeFrameView?.frame = barCodeObject!.bounds
+            
+            if metadataObj.stringValue != nil {
+                messageLabel.text = metadataObj.stringValue
+            }
+        }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Leitor QRCODE/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Leitor QRCODE
+//
+//  Created by Marcos Felipe Souza on 29/12/16.
+//  Copyright © 2016 Marcos. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }      
+}
+
